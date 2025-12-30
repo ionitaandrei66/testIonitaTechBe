c028f2115747e44d30979cfa300dbcf64f5ad057
ionitaandrei66 <ionita_andrei_2002@yahoo.com>
2025-12-30 17:55:43 +0200

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,24 @@
 import {
   Body,
-  Controller,
+  Controller, Get,
   HttpCode,
   HttpStatus,
-  Post,
-  Res,
-} from '@nestjs/common';
+  Post, Req,
+  Res, UnauthorizedException
+} from "@nestjs/common";
 import { AuthService } from './services/auth.service';
 import { Response } from 'express';
 import { UserAuthDTO } from './models/user-auth-d-t.o';
+import { JwtService } from "@nestjs/jwt";
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private jwtService: JwtService) {}
+
+  public decodeJWT(codedJWT: string): string | { [key: string]: any } {
+    const jwt = codedJWT.replace('Bearer ', '');
+    return this.jwtService.decode(jwt);
+  }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
@@ -21,5 +27,19 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ) {
     return await this.authService.handleLogin(req, res);
+  }
+
+
+  @Get('check')
+  checkAuth(@Req() req: any) {
+    const token = req.cookies?.['access_token'];
+    if (!token) throw new UnauthorizedException('No token');
+
+    try {
+      const decoded = this.decodeJWT(token);
+      if (decoded) return { authenticated: true };
+    } catch (err) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
   }
 }
